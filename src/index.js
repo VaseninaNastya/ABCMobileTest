9f7900df9b9b3f18c19bfe97a7cb8d35043301e1
VaseninaNastya <a.vasenina@andersenlab.com>
2022-03-04 22:06:57 +0300

    Feat: init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import MainBlock from "./components/MainBlock/MainBlock";
+
+(async () => {
+  const mainBlock = new MainBlock();
+  const mainBlockElem = await mainBlock.generateLayout();
+  document.body.prepend(mainBlockElem);
+})();
